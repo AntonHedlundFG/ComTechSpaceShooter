cb6f885a5f16fc35a5545e09d782eef86a86a1bc
AntonHedlundFG <Anton.Hedlund@edu.futuregames.nu>
2023-10-17 09:45:44 +0200

    Increase bullet & asteroid count

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +10,8 @@ public class Gun : MonoBehaviour
     [SerializeField] [Range(1.0f, 50.0f)] private float _bulletSpeed = 10.0f;
     [SerializeField] [Range(1.0f, 10.0f)] private float _damage = 1.0f;
     [SerializeField] [Range(5.0f, 20.0f)] private float _duration = 10.0f;
+    [SerializeField] [Range(1, 10)] private int _bulletCount = 3;
+    [SerializeField] [Range(0.0f, 45.0f)] private float _bulletSpread = 5.0f;
 
 
     private float _lastFireTime;
@@ -33,14 +34,20 @@ public class Gun : MonoBehaviour
     {
         if (_bulletPrefab == null) return;
 
-        GameObject _newObject = Instantiate(_bulletPrefab);
-        Bullet _newBullet = _newObject.GetComponent<Bullet>();
-        if (_newBullet == null)
+        for (int i = 0; i < _bulletCount; i++)
         {
-            Destroy(_newObject);
-            return;
+            GameObject _newObject = Instantiate(_bulletPrefab);
+            Bullet _newBullet = _newObject.GetComponent<Bullet>();
+            if (_newBullet == null)
+            {
+                Destroy(_newObject);
+                return;
+            }
+            Vector3 velocity = transform.right * _bulletSpeed;
+            float spread = Random.Range(-_bulletSpread, _bulletSpread);
+            velocity = Quaternion.Euler(new Vector3(0, 0, spread)) * velocity;
+            _newBullet.SetupBullet(transform.position, velocity, _damage, _duration);
         }
-        _newBullet.SetupBullet(transform.position, transform.right * _bulletSpeed, _damage, _duration);
 
     }
 }
